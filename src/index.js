743c690eaba3773c10c44700b16f1356c5aae3b9
Jose Caceres <josecaceres.oreul@gmail.com>
2020-05-27 22:04:13 -0500

    Refactor

--- src/index.js
+++ src/index.js
@@ -8,11 +8,8 @@ var config_1 = __importDefault(require("./config"));
 var modules_1 = require("./modules");
 exports.config = function (credentials) {
     var _a = credentials.X_Login, X_Login = _a === void 0 ? null : _a, _b = credentials.X_Trans_Key, X_Trans_Key = _b === void 0 ? null : _b, _c = credentials.secret, secret = _c === void 0 ? null : _c, _d = credentials.env, env = _d === void 0 ? null : _d;
-    if (!X_Login || !X_Trans_Key || !secret || !env)
-        throw ("Invalid credentials");
-    if (env !== 'qa' && env !== 'prod')
-        throw ("Invalid env");
-    config_1.default.getInstance().setCredentials(credentials);
+    if (X_Login && X_Trans_Key && secret && env && (env === 'qa' || env === 'prod'))
+        config_1.default.getInstance().setCredentials(credentials);
 };
 exports.default = {
     config: exports.config,
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,8 @@ import {wallets} from './modules';
 
 export const config = (credentials: any) => {
     const {X_Login = null, X_Trans_Key = null, secret = null, env = null} = credentials;
-
-    if (!X_Login || !X_Trans_Key || !secret || !env)
-        throw("Invalid credentials")
-
-    if (env !== 'qa' && env !== 'prod')
-        throw("Invalid env")
-
-    Configuration.getInstance().setCredentials(credentials);
+    if (X_Login && X_Trans_Key && secret && env && (env === 'qa' || env === 'prod'))
+        Configuration.getInstance().setCredentials(credentials);
 };
 
 export default {
