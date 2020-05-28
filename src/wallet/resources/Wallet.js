50483568f95f4b757446386090d55fc8c91d7371
Jose Caceres <josecaceres.oreul@gmail.com>
2020-05-27 21:25:45 -0500

    Se agrega el proveedor para las transacciones

--- src/wallet/resources/Wallet.js
+++ src/wallet/resources/Wallet.js
@@ -184,102 +184,86 @@ var Wallet = /** @class */ (function (_super) {
     };
     Wallet.prototype.recharge = function (payload) {
         var _this = this;
-        return this.createTransaction(function (resolve, reject) {
-            if (resolve === void 0) { resolve = function () { }; }
-            if (reject === void 0) { reject = function () { }; }
-            return __awaiter(_this, void 0, void 0, function () {
-                var response;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, providers_1.transactionsProvider.createRecharge(__assign({ wallet: this.uuid, transactions_type: 'recharge' }, payload))];
-                        case 1:
-                            response = _a.sent();
-                            response.error ?
-                                reject(response.error) :
-                                resolve(response);
-                            return [2 /*return*/];
-                    }
-                });
+        return this.createTransaction(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
+            var response;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, providers_1.transactionsProvider.createRecharge(__assign({ wallet: this.uuid, transactions_type: 'recharge' }, payload))];
+                    case 1:
+                        response = _a.sent();
+                        response.error ?
+                            reject(response.error) :
+                            resolve(response);
+                        return [2 /*return*/];
+                }
             });
-        });
+        }); });
     };
     Wallet.prototype.purchase = function (payload) {
         var _this = this;
-        return this.createTransaction(function (resolve, reject) {
-            if (resolve === void 0) { resolve = function () { }; }
-            if (reject === void 0) { reject = function () { }; }
-            return __awaiter(_this, void 0, void 0, function () {
-                var response, balances;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, providers_1.transactionsProvider.createRecharge(__assign({ wallet: this.uuid, transactions_type: 'purchase' }, payload))];
-                        case 1:
-                            response = _a.sent();
-                            if (response.error) {
-                                reject(response.error);
-                            }
-                            else {
-                                balances = response.transaction.attributes.sender_wallet.balances;
-                                this.balances = balances;
-                                resolve(response);
-                            }
-                            return [2 /*return*/];
-                    }
-                });
+        return this.createTransaction(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
+            var response, balances;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, providers_1.transactionsProvider.createRecharge(__assign({ wallet: this.uuid, transactions_type: 'purchase' }, payload))];
+                    case 1:
+                        response = _a.sent();
+                        if (response.error) {
+                            reject(response.error);
+                        }
+                        else {
+                            balances = response.transaction.attributes.sender_wallet.balances;
+                            this.balances = balances;
+                            resolve(response);
+                        }
+                        return [2 /*return*/];
+                }
             });
-        });
+        }); });
     };
     Wallet.prototype.withdrawal = function (payload) {
         var _this = this;
-        return this.createTransaction(function (resolve, reject) {
-            if (resolve === void 0) { resolve = function () { }; }
-            if (reject === void 0) { reject = function () { }; }
-            return __awaiter(_this, void 0, void 0, function () {
-                var response, balances;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, providers_1.transactionsProvider.createRecharge(__assign({ wallet: this.uuid, transactions_type: 'withdrawal' }, payload))];
-                        case 1:
-                            response = _a.sent();
-                            if (response.error) {
-                                reject(response.error);
-                            }
-                            else {
-                                balances = response.transaction.attributes.sender_wallet.balances;
-                                this.balances = balances;
-                                resolve(response);
-                            }
-                            return [2 /*return*/];
-                    }
-                });
+        return this.createTransaction(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
+            var response, balances;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, providers_1.transactionsProvider.createRecharge(__assign({ wallet: this.uuid, transactions_type: 'withdrawal' }, payload))];
+                    case 1:
+                        response = _a.sent();
+                        if (response.error) {
+                            reject(response.error);
+                        }
+                        else {
+                            balances = response.transaction.attributes.sender_wallet.balances;
+                            this.balances = balances;
+                            resolve(response);
+                        }
+                        return [2 /*return*/];
+                }
             });
-        });
+        }); });
     };
     Wallet.prototype.send = function (payload) {
         var _this = this;
-        return this.createTransaction(function (resolve, reject) {
-            if (resolve === void 0) { resolve = function () { }; }
-            if (reject === void 0) { reject = function () { }; }
-            return __awaiter(_this, void 0, void 0, function () {
-                var response, balances;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0: return [4 /*yield*/, providers_1.transactionsProvider.createRecharge(__assign({ wallet: this.uuid, transactions_type: 'send' }, payload))];
-                        case 1:
-                            response = _a.sent();
-                            if (response.error) {
-                                reject(response.error);
-                            }
-                            else {
-                                balances = response.transaction.attributes.sender_wallet.balances;
-                                this.balances = balances;
-                                resolve(response);
-                            }
-                            return [2 /*return*/];
-                    }
-                });
+        return this.createTransaction(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
+            var response, balances;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, providers_1.transactionsProvider.createRecharge(__assign({ wallet: this.uuid, transactions_type: 'send' }, payload))];
+                    case 1:
+                        response = _a.sent();
+                        if (response.error) {
+                            reject(response.error);
+                        }
+                        else {
+                            balances = response.transaction.attributes.sender_wallet.balances;
+                            this.balances = balances;
+                            resolve(response);
+                        }
+                        return [2 /*return*/];
+                }
             });
-        });
+        }); });
     };
     return Wallet;
 }(BaseResource_1.default));
